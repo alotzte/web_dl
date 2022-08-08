bf092bf88b28fad4e246f63c08c6afec197aa2ce
alotzte <71650164+alotzte@users.noreply.github.com>
2022-08-08 20:03:36 +0300

    edit place

--- main_app/sub_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import SkinModel
-
-class SkinForm(forms.ModelForm):
-	class Meta:
-		model=SkinModel
-		fields = "__all__"
--- main_app/sub_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.home,name='home'),
-    # path('upload',views.uploadImage, name='uploadImage'),
-    #path('upoadbyURL',views.uploadURL, name='uploadURL')
-    ]
